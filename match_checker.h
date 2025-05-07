88014b475d40041354a4b222576ff273a1ac46bc
Coff3e <pankeev-02@mail.ru>
2025-05-07 15:48:19 +0300

    Добавлены первые функции для проверки

--- match_checker.h
+++ match_checker.h
@@ -1,6 +1,16 @@
 #pragma once
+
+#include <vector>
+
 class match_checker
 {
+public:
+	bool check_match();
 
+private:
+	bool check_three_in_a_row();
+
+	bool check_three_in_a_col();
+
+	bool has_three_consecutive_equal(const std::vector<int>& row);
 };
-
